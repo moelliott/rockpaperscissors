299b91245e1f2b5fe30093dfa8a1cc1f5328e2c4
Glitch (white-snow-flakes) <none>
2021-11-17 00:21:58 +0000

    🚣🌸 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-$('#shoot').click(function(){
+$('.play').click(function(){
 
-  let shootValue = $('#imput').val() 
+  let shootValue = $('.input').val() 
+  let computerChoice;
+  let randomNumber;
   
-  $
+  $(".userChoice").text(shootValue)
+  
+  $(".computerChoice").text(shootValue)
+  if(randomNumber === 1) {
+	computerChoice = 'rock';
 }
+
+if(randomNumber === 2) {
+	computerChoice = 'paper';
+}
+
+if(randomNumber === 3) {
+	computerChoice = 'scissors';
+}
+})
