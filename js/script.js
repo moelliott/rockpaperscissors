c555312001bc33b44bff8f443e3f6f0a3e8b6fc8
Glitch (white-snow-flakes) <none>
2022-01-12 00:30:25 +0000

    🚤📭 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-$(".play").click(function() {
+$(".play").click(function () {
   let userChoice = $(".input").val();
 
   $(".userChoice").text(userChoice);
   let randomNumber = Math.ceil(Math.random() * 3);
   let computerChoice = randomNumber;
-
   if (randomNumber === 2) {
     computerChoice = "rock";
-    $(".result").text("Good job!");
+    
   } else if (randomNumber === 3) {
     computerChoice = "paper";
-    $(".result").text("You won!");
+    
   } else if (randomNumber === 1) {
     computerChoice = "scissors";
-    $(".result").text("Winner!");
+    
   }
   $(".computerChoice").text(computerChoice);
-});
+
+  if (computerChoice === "scissors" && userChoice === "rock") {
+
+
+  }
